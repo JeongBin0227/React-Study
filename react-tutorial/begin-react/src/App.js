c8d321381f346d0da4c0058a50b458c6a71e698c
KimJeongBin <kjh3095000@gmail.com>
2020-02-17 19:56:43 +0900

    [ADD] reactStudy - 02.17

---
+++ react-tutorial/begin-react/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Hello from "./Hello.js";
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Hello />
+      <Hello />
+      <input></input>
+      <br></br>
+    </div>
+  );
+}
+
+export default App;
