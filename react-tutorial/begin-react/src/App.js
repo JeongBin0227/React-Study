fdb905e31ec9d0d65bc6c0d7f2a701bafdd8e628
JeongBin <kjh3095000@gmail.com>
2020-03-03 22:15:51 +0900

    [ADD] callback 함수를 이용하여 최적화

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,6 +1,12 @@
-import React, {useState, useRef} from "react";
+import React, {useState, useRef, useMemo, useCallback} from "react";
 import UserList from './UserList.js';
 import CreateUser from './createUser.js'
+
+function countActiveUsers(users){
+  console.log("활성 사용자 수를 세는중..")
+  return users.filter(user=>user.active).length
+}
+
 function App() {
   const [inputs, setInputs] = useState({
     username:'',
@@ -8,62 +14,60 @@ function App() {
   });
   const {username,email} = inputs;
 
-  const onChange = (e) => {
+  const onChange = useCallback((e) => {
     const {name, value} = e.target;
         setInputs({
             ...inputs,
             [name] : value,
         });
 
-  }
+  })
   const [users,setUsers] = useState([
     {
         id : 1,
-        username : 'a',
-        email:'jh3095000',
-        active:true,
+        username : 'David',
+        email:'david@naver.com',
+        active:false,
     },
     {
         id : 2,
-        username : 'b',
-        email:'zd2231',
+        username : 'kavin',
+        email:'kavin@naver.com',
         active:false,
     },
     {
         id : 3,
-        username : 'c',
-        email:'dasdasd',
+        username : 'kate',
+        email:'kate@naver.com',
         active:false,
     },
   ])
   const nextId = useRef(4);
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user ={
       id:nextId.current,
       username,
       email
     }
-    setUsers(users.concat(user))
-    console.log(users)
-    setUsers([...users,user])
-    console.log(users)
-
+    setUsers(users=>users.concat(user))
     setInputs({
       username:'',
       email:''
 
   });
     nextId.current+=1;
-  }
+  },[username,email])
 
-  const onDelite=(id)=>{
-    setUsers(users.filter(users => users.id !== id))
-  }
+  const onDelite=useCallback((id)=>{
+    setUsers(users => users.filter(users => users.id !== id))
+  },[])
 
-  const onToggle=(id)=>{
-    setUsers(users.map(user => user.id===id?{...user,active:!user.active}:user))
-  }
+  const onToggle=useCallback((id)=>{
+    setUsers(users => users.map(user => user.id===id?{...user,active:!user.active}:user))
+  },[])
+  
+  const count = useMemo(()=>countActiveUsers(users),[users]);
   return (
     <div>
       <CreateUser username={username} email={email} onChange={onChange} onCreate = {onCreate}/>
@@ -71,6 +75,7 @@ function App() {
       <b>{users.length}</b>
       <UserList users={users} onDelite={onDelite} onToggle={onToggle}/>
       <br></br>
+      {count}
     </div>
   );
 }
--- react-tutorial/begin-react/src/UserList.js
+++ react-tutorial/begin-react/src/UserList.js
@@ -1,40 +1,22 @@
-import React from 'react'
-function User({user, onDelite,onToggle}){
+import React,{useEffect} from 'react'
+const User = React.memo(function User({user, onDelite,onToggle}){
     return(
         <div>
             <b onClick={()=>onToggle(user.id)}style={{color: user.active?'red':'black'}}>{user.username}</b> <span>({user.email})</span>
             <button onClick={()=>onDelite(user.id)}>삭제</button>
         </div>
     )
-}
-function UserList({users, onDelite,onToggle}){
-    // const users = [
-    //     {
-    //         id : 1,
-    //         username : 'a',
-    //         email:'jh3095000',
-    //     },
-    //     {
-    //         id : 2,
-    //         username : 'b',
-    //         email:'zd2231',
-    //     },
-    //     {
-    //         id : 3,
-    //         username : 'c',
-    //         email:'dasdasd',
-    //     },
-    // ]
-
+})
+const UserList = React.memo(function UserList({users, onDelite,onToggle}){
     return(
        <div>
            {
            users.map(
-            users => (<User user={users} onDelite={onDelite} onToggle={onToggle}/>)
+            users => (<User user={users} key={users.id}onDelite={onDelite} onToggle={onToggle}/>)
            )
             }   
        </div>
     )
-}
+})
 
-export default UserList;
+export default React.memo(UserList);
