9e052668f49375f39d6a42dbb7cccff59566dc7c
JeongBin <kjh3095000@gmail.com>
2020-03-06 13:44:16 +0900

    [ADD] prettier, eslint

--- react-tutorial/begin-react/src/App.js
+++ react-tutorial/begin-react/src/App.js
@@ -1,107 +1,121 @@
-import React, {useReducer, useRef, useMemo, useCallback, useState} from "react";
+import React, {
+    useReducer,
+    useRef,
+    useMemo,
+    useCallback,
+    useState,
+    useEffect,
+} from 'react';
 import UserList from './UserList.js';
-import CreateUser from './createUser.js'
+import CreateUser from './createUser.js';
 
-function countActiveUsers(users){
-  console.log("활성 사용자 수를 세는중..")
-  return users.filter(user=>user.active).length
+function countActiveUsers(users) {
+    console.log('활성 사용자 수를 세는중..');
+    return users.filter(user => user.active).length;
 }
 
 const intialState = {
-  inputs:{
-    username:'',
-    email:'',
-  },
-  nextId:4,
-  users:[
-    {
-        id : 1,
-        username : 'David',
-        email:'david@naver.com',
-        active:false,
+    inputs: {
+        username: '',
+        email: '',
     },
-    {
-        id : 2,
-        username : 'kavin',
-        email:'kavin@naver.com',
-        active:false,
-    },
-    {
-        id : 3,
-        username : 'kate',
-        email:'kate@naver.com',
-        active:false,
-    },
-  ]
-}
-function reducer(state,action){
-	switch(action.type){
-		case 'CHANGE_VALUE':
-			return {
-        ...state,
-        inputs:{
-          ...state.inputs,
-          [action.name] : action.value
-        }
-      };
-    case 'CREATE_VALUE':
-      return{
-        inputs:intialState.inputs,
-        users : state.users.concat(action.user)
-      };
-    case 'DELITE_VALUE':
-      return{
-        inputs:intialState.inputs,
-        users : state.users.concat(action.user)
-      };
-		defalut:
-			throw new Error('unhandled action')
-	}
+    nextId: 4,
+    users: [
+        {
+            id: 1,
+            username: 'David',
+            email: 'david@naver.com',
+            active: false,
+        },
+        {
+            id: 2,
+            username: 'kavin',
+            email: 'kavin@naver.com',
+            active: false,
+        },
+        {
+            id: 3,
+            username: 'kate',
+            email: 'kate@naver.com',
+            active: false,
+        },
+    ],
+};
+function reducer(state, action) {
+    switch (action.type) {
+        case 'CHANGE_VALUE':
+            return {
+                ...state,
+                inputs: {
+                    ...state.inputs,
+                    [action.name]: action.value,
+                },
+            };
+        case 'CREATE_VALUE':
+            return {
+                inputs: intialState.inputs,
+                users: state.users.concat(action.user),
+            };
+        case 'DELITE_VALUE':
+            return {
+                inputs: intialState.inputs,
+                users: state.users.concat(action.user),
+            };
+            defalut: throw new Error('unhandled action');
+    }
 }
 function App() {
-  const [state, dispatch ] = useReducer(reducer,intialState);
-  const {users} = state;
-  const {username,email} = state.inputs;
-  const nextId = useRef(4)
-  
-  const onChange= useCallback(e=>{
-    const {name,value} = e.target
-    dispatch({
-      type:'CHANGE_VALUE',
-      name,
-      value
-    })
-  },[])
+    const [value, setValue] = useState(0);
+    useEffect(() => {
+        console.log(value);
+    }, []);
+    const [state, dispatch] = useReducer(reducer, intialState);
+    const { users } = state;
+    const { username, email } = state.inputs;
+    const nextId = useRef(4);
 
-  const onCreate= useCallback(()=>{
-    const user={
-        id:nextId.current,
-        username,
-        email
-    }
-    dispatch({
-      type:'CREATE_VALUE',
-      user
-    })
-    nextId.current++
-  },[username,email])
+    const onChange = useCallback(e => {
+        const { name, value } = e.target;
+        dispatch({
+            type: 'CHANGE_VALUE',
+            name,
+            value,
+        });
+    }, []);
 
-  const onDelite = useCallback((id)=>{
-    dispatch({
-      type:'DELITE_VALUE',
-      id
-    })
-  },[])
+    const onCreate = useCallback(() => {
+        const user = {
+            id: nextId.current,
+            username,
+            email,
+        };
+        dispatch({
+            type: 'CREATE_VALUE',
+            user,
+        });
+        nextId.current++;
+    }, [username, email]);
 
-  return (
-    <div>
-      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}/>
-      <br></br>
-      <UserList users={users} onDelite={} onToggle={} />
-      <br></br>
-      0
-    </div>
-  );
+    const onDelite = useCallback(id => {
+        dispatch({
+            type: 'DELITE_VALUE',
+            id,
+        });
+    }, []);
+
+    return (
+        <div>
+            <CreateUser
+                username={username}
+                email={email}
+                onChange={onChange}
+                onCreate={onCreate}
+            />
+            <br></br>
+            <UserList users={users} onDelite={} onToggle={} />
+            <br></br>0
+        </div>
+    );
 }
 
 export default App;
