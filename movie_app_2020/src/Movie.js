2bc4aa0dfbd9a183d7a914033d2bba380948d354
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-06-09 22:37:06 +0900

    [ADD] 노마더 코더_movie app 완료

--- movie_app_2020/src/Movie.js
+++ movie_app_2020/src/Movie.js
@@ -1,8 +1,26 @@
+import "./Movie.css";
+
 import PropTypes from "prop-types";
 import React from "react";
 
-function Movie({ id, year, title, summary, poster }) {
-  return <h1>{title}</h1>;
+function Movie({ id, year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title} />
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <h5 className="movie__year">{year}</h5>
+        <ul className="genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <p className="movie__summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
@@ -11,6 +29,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes).isRequired,
 };
 
 export default Movie;
