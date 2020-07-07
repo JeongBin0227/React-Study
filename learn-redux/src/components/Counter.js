9675687568c7b60f56657fef4db42c98e6cb3431
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-07-07 18:25:55 +0900

    [EDIT] Counter 컴포넌트 수정

--- learn-redux/src/components/Counter.js
+++ learn-redux/src/components/Counter.js
@@ -1,15 +1,22 @@
 import React from 'react'
 
-function Counter(number, diff, onIncrese, onDecrease, onSetDiff){
+function Counter( { number, diff, onIncrease, onDecrease, onSetDiff } ){
+    console.log(number)
     const onChange = e => {
+        console.log(e.target.value)
         onSetDiff(parseInt(e.target.value,10))
     }
 
     return(
         <div>
-            <input type='number' value={diff} onChange={onChange}/>
-            <button onClick={onIncrese} >+</button>
-            <button onClick={onDecrease}>-</button>
+            <h1>{number}</h1>
+            <div>
+                <input type='number' value={diff} onChange={onChange}/>
+                <button onClick={onIncrease} >+</button>
+                <button onClick={onDecrease}>-</button>
+            </div>
         </div>
     )
 }
+
+export default Counter
