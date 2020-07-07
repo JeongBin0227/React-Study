4448cb95942b00b1a7d7ac83b74dc847c06f2e52
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-07-07 17:28:24 +0900

    [ADD] Reducer module 작성

---
+++ learn-redux/src/modules/counter.js
@@ -0,0 +1,34 @@
+const initialState = {
+    counter : 0,
+    diff:1,
+}
+
+const INCREASE = 'counter/INCREASE'
+const DECREASE = 'counter/DECREASE'
+const SET_DIFF = 'counter/SET_DIFF'
+
+export const increase = () => ({type: INCREASE})
+export const decrease = () => ({type: DECREASE})
+export const set_diff = (diff) => ({type: SET_DIFF, diff})
+
+export default function counter(state = initialState, action) {
+    switch(action.type){
+        case INCREASE:
+            return{
+                ...state,
+                counter: state.counter + state.diff
+            }
+        case DECREASE:
+            return{
+                ...state,
+                counter: state.counter - state.diff
+            }
+        case SET_DIFF:
+            return{
+                ...state,
+                diff: action.diff
+            }
+        default:
+            return state
+    }
+}
