d2390c36b4bdef34b49d2061c4108fd5650c61e0
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-08-04 15:39:46 +0900

    [ADD] redux-thunk 설치 및 준비

--- learn-redux/src/components/Todos.js
+++ learn-redux/src/components/Todos.js
@@ -1,11 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 
 function TodoItem({ todo, onToggle }) {
-    return(
-        <li style={{ textDecoration=todo.done ? 'line-throught':'none'}} onClick={() => onToggle(todo.id)}>
-            {todo.text}
-        </li>
-    )
+  return (
+    <li
+      style={{ textDecoration: todo.done ? "line-through" : "none" }}
+      onClick={() => onToggle(todo.id)}
+    >
+      {todo.text}
+    </li>
+  )
 }
 
 function TodoList({ todos, onToggle }) {
@@ -22,12 +25,16 @@ function Todos({ todos, onCreate, onToggle }) {
   const onChange = (e) => {
     setText(e.target.value)
   }
-  const onSubmit = (e) => [e.preventDefalut(), onCreate(text), setText("")]
+  const onSubmit = (e) => {
+    e.preventDefault()
+    onCreate(text)
+    setText("")
+  }
 
   return (
     <div>
-      <form onSubmit={onSubmit()}>
-        <input value={text} onChange={onChange} placeholder="할일은 적으세요" />
+      <form onSubmit={onSubmit}>
+        <input value={text} onChange={onChange} placeholder="할일을 적으세요" />
         <button type="submit">추가</button>
       </form>
       <TodoList todos={todos} onToggle={onToggle} />
