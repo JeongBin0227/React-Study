0c6fea7f0d65d3032a7852ff0320061a8fd1c003
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-08-04 16:26:27 +0900

    [ADD] reducer utils 작성

--- learn-redux-middleware/src/modules/posts.js
+++ learn-redux-middleware/src/modules/posts.js
@@ -1,4 +1,5 @@
 import * as postsAPI from '../api/posts'
+import { reducerUtils } from '../lib/asyncUtils'
 
 const GET_POSTS = 'GET_POSTS'
 const GET_POSTS_SUCESS = 'GET_POSTS_SUCESS'
@@ -46,4 +47,54 @@ export const getPost = (id) => async dispatch => {
             error:error
         })
     }
+}
+
+const initialState = { 
+    posts:reducerUtils.initial(),
+    post: reducerUtils.initial()
+}
+
+export default function posts(state=initialState, action) {
+    switch(action.type) {
+        case GET_POSTS:{
+            return {
+                ...state,
+                posts:reducerUtils.loading()
+            }
+        }
+        case GET_POSTS_SUCESS:{
+            return {
+                ...state,
+                posts:reducerUtils.success(action.posts)
+            }
+        }
+        case GET_POSTS_ERROR:{
+            return {
+                ...state,
+                post:reducerUtils.error(action.error)
+            }
+        }
+        case GET_POST:{
+            return {
+                ...state,
+                post:reducerUtils.loading()
+            }
+        }
+        case GET_POST_SUCESS:{
+            return {
+                ...state,
+                post:reducerUtils.success(action.post)
+            }
+        }
+        case GET_POST_ERROR:{
+            return {
+                ...state,
+                posts:reducerUtils.error(action.error)
+            }
+        }
+        default:
+            return state
+    }
+
+    return state
 }
