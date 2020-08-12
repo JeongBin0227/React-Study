5b35a790a59df288be3a2a10b80c9d08653d2c46
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2020-08-12 18:38:16 +0900

    [ADD] PostList 페이지 구현

---
+++ learn-redux-middleware/src/pages/PostListPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PostListContainer from '../containers/PostListContainer';
+
+function PostPage() {
+    // const {id} = match.params
+    // const postId = parseInt(id,10)
+    console.log('sda')
+    return (
+        <PostListContainer/>
+    );
+}
+
+export default PostPage;
