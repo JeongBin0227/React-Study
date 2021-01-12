6e7b0023c1c59a286b8bf28d9a9fb9871beae7b5
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-01-12 23:45:47 +0900

    feat: 똑딱 프로젝트 컴포넌트 테스터

---
+++ make-components/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./SelectorBox.css";
+import Professor from "./components/Professor/index";
+
+function App() {
+  return (
+    <div className="App">
+      <Professor></Professor>
+    </div>
+  );
+}
+
+export default App;
