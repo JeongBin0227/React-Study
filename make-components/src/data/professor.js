df0c4c4886f05a9f884c18f6cf8c33a5f665f759
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-01-12 23:46:47 +0900

    feat: 교수 목 데이터

---
+++ make-components/src/data/professor.js
@@ -0,0 +1,49 @@
+const professors = [
+  {
+    id: 1,
+    position: "교수",
+    school: "중앙대학교",
+    department: "데이터 베이스 시스템",
+    email: "jh3095@naver.com",
+    url: "https://www.naver.com",
+    name: "김정빈",
+  },
+  {
+    id: 2,
+    position: "교수",
+    school: "중앙대학교",
+    department: "데이터 베이스 시스템",
+    email: "jh3095@naver.com",
+    url: "https://www.naver.com",
+    name: "김정빈",
+  },
+  {
+    id: 3,
+    position: "교수",
+    school: "중앙대학교",
+    department: "데이터 베이스 시스템",
+    email: "jh3095@naver.com",
+    url: "https://www.naver.com",
+    name: "김정빈",
+  },
+  {
+    id: 4,
+    position: "교수",
+    school: "중앙대학교",
+    department: "데이터 베이스 시스템",
+    email: "jh3095@naver.com",
+    url: "https://www.naver.com",
+    name: "김정빈",
+  },
+  {
+    id: 5,
+    position: "교수",
+    school: "중앙대학교",
+    department: "데이터 베이스 시스템",
+    email: "jh3095@naver.com",
+    url: "https://www.naver.com",
+    name: "김정빈",
+  },
+];
+
+export default professors;
