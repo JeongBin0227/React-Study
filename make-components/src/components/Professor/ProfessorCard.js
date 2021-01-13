7734ded2763d98ae0404df0e575a2547a2ae7cdd
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-01-13 23:59:17 +0900

    feat: professor card styledcomponent

--- make-components/src/components/Professor/ProfessorCard.js
+++ make-components/src/components/Professor/ProfessorCard.js
@@ -1,12 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+
+const Card = styled.a`
+  display: inline-block;
+  border-radius: 10px;
+  width: 100px;
+  height: 150px;
+  padding: 10px;
+  border: 2px solid white;
+`;
 
 const ProfessorCard = ({ professor }) => {
   console.log(professor);
   return (
-    <div className="professr_card">
+    <Card className="professr_card">
       <span>{professor.name}</span>
       <span>{professor.position}</span>
-    </div>
+      <span>{professor.position}</span>
+      <span>{professor.position}</span>
+      <span>{professor.position}</span>
+    </Card>
   );
 };
 
