ee3b31dba25fbd0064faf534c47fed016a5bb8d5
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-01-14 21:01:44 +0900

    feat: Professor Component

--- make-components/src/components/Professor/ProfessorCard.js
+++ make-components/src/components/Professor/ProfessorCard.js
@@ -1,10 +1,10 @@
 import React from "react";
 import styled from "styled-components";
+import SVG from "../../../src/assets/emailPlus.svg";
 
-const Card = styled.a`
+const Card = styled.div`
   display: inline-block;
   border-radius: 10px;
-  width: 100px;
   height: 150px;
   padding: 10px;
   border: 2px solid white;
@@ -13,12 +13,19 @@ const Card = styled.a`
 const ProfessorCard = ({ professor }) => {
   console.log(professor);
   return (
-    <Card className="professr_card">
-      <span>{professor.name}</span>
-      <span>{professor.position}</span>
-      <span>{professor.position}</span>
-      <span>{professor.position}</span>
-      <span>{professor.position}</span>
+    <Card className="professor_card">
+      <div className="professor">
+        <div className="professor-header">
+          <span className="professor-name">{professor.name}</span>
+          <span className="professor-position">{professor.position}</span>
+          <a href={professor.url}>
+            <img src={SVG} class="search-icon" alt="search"></img>
+          </a>
+        </div>
+        <div className="professor-school">{professor.school}</div>
+        <div className="professor-position">{professor.department}</div>
+        <div className="professor-position">{professor.email}</div>
+      </div>
     </Card>
   );
 };
