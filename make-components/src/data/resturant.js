8611606929ab80db862b77c2297cb1a61b539e70
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-01-20 23:53:26 +0900

    feat: resturants 데이터 정리

---
+++ make-components/src/data/resturant.js
@@ -0,0 +1,45 @@
+const resturants = [
+  {
+    id: 1,
+    name: "라이스&포테이토",
+    explanation: "김치철판볶음밥 등 최고의 맛집",
+    imgae: [
+      "https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDExMDhfNTAg%2FMDAxNjA0ODI5MjM2NDIx.F-ERMZ13AG76tR53XfCALCHwqXB0nAa7Y0AximDj6nAg.sjW0-KvmfWWZG7qMv3D6_Dk2fkofZkf_lZUY1qmTWRIg.JPEG.dwk0708%2Foutput_643343784.jpg",
+      "https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDExMDhfNTAg%2FMDAxNjA0ODI5MjM2NDIx.F-ERMZ13AG76tR53XfCALCHwqXB0nAa7Y0AximDj6nAg.sjW0-KvmfWWZG7qMv3D6_Dk2fkofZkf_lZUY1qmTWRIg.JPEG.dwk0708%2Foutput_643343784.jpg",
+      "https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDExMDhfNTAg%2FMDAxNjA0ODI5MjM2NDIx.F-ERMZ13AG76tR53XfCALCHwqXB0nAa7Y0AximDj6nAg.sjW0-KvmfWWZG7qMv3D6_Dk2fkofZkf_lZUY1qmTWRIg.JPEG.dwk0708%2Foutput_643343784.jpg",
+    ],
+    priceImage:
+      "https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDA0MDlfMjAx%2FMDAxNTg2NDM1Njc1MDY3.YAnsztrIAI4AkwPYxA8tqDWz1FL45lJFvoD8moM1wyUg.-iv3E1aJFrirlDUsyb2btNq5e7l2R29jctPXLjEhMAMg.JPEG.op159159%2FIMG_8590.JPG",
+    homepage: "naver.com",
+    position: "서울 동작구 흑석로 83",
+    startTime: "11:00",
+    endTime: "23:00",
+    xCoordinate: 15.0,
+    xCoordinate: 16.0,
+    reviews: [
+      {
+        id: 1,
+        author: "김정빈",
+        time: "201221",
+        star: 4,
+        comment: "맛있어요",
+      },
+      {
+        id: 2,
+        author: "최범수",
+        time: "201221",
+        star: 4,
+        comment: "별로",
+      },
+      {
+        id: 3,
+        author: "유인근",
+        time: "201221",
+        star: 4,
+        comment: "맛있어요",
+      },
+    ],
+  },
+];
+
+export default resturants;
