8f99dc71fd6fe00f3d82d86c12f958a30e100f2a
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-01-23 23:57:48 +0900

    feat: restaurantInfo component

---
+++ make-components/src/components/Restaurant/RestaurantInfo.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const RestaurantInfo = ({}) => {
+  return (
+    <div className="restaurantTitle">
+      <div className="main">
+        <a>
+          <h1 className="title">라이스&포테이토</h1>
+        </a>
+        <span className="subjectTitle">한식</span>
+      </div>
+      <div className="sub">
+        <span>김치철판볶음밥 등 최고의 맛집</span>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantInfo;
