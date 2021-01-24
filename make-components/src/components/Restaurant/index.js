f012ff3a4679b1903c2e9e664fb116c1f51478a4
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-01-25 02:16:47 +0900

    Revert "feat: restaurant Index component"
    This reverts commit 942bdd716d6a7add12b2f623fe22532d5eb62a4a.

--- make-components/src/components/Restaurant/index.js
+++ make-components/src/components/Restaurant/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import SearchBar from "./SearchBar";
-import RestaurantInfo from "./RestaurantInfo";
-import RestaurantTab from "./RestaurantTab";
 
 const NaverMap = styled.div`
   width:150px
@@ -16,9 +13,12 @@ const index = () => {
   return (
     <div className="restaurant">
       <div>
-        <SearchBar></SearchBar>
-        <RestaurantInfo></RestaurantInfo>
-        <RestaurantTab></RestaurantTab>
+        <div className="menu">
+          <div>홈</div>
+          <div>메뉴</div>
+          <div>리뷰</div>
+          <div>사진</div>
+        </div>
       </div>
       <NaverMap className="naverMap"></NaverMap>
     </div>
