89edd73642a9c5e8b99b304961fc6884712ff908
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-01-30 23:58:18 +0900

    feat: restuarant search bar

--- make-components/src/components/Restaurant/SearchBar.js
+++ make-components/src/components/Restaurant/SearchBar.js
@@ -3,11 +3,24 @@ import SVG from "../../../src/assets/search.svg";
 
 const SearchBar = () => {
   return (
-    <>
-      <input type="text" placeholder="Search.."></input>
-      <img src={SVG} class="search-icon" alt="search"></img>
-    </>
+    <div className="RestaurantSearch">
+      <div class="search_wrap">
+        <div class="search_box">
+          {/* <button type="button" class="button_search"> 검색 </button> */}
+          <img src={SVG} class="search-icon" alt="search"></img>
+          <div class="input_box">
+            {/* <label class="label_search ng-star-inserted" for="input_search1611765486961"> 장소, 버스, 지하철, 도로 검색 </label> */}
+            {/* <input type="text" autocomplete="off" autofocus="" class="input_search ng-pristine ng-valid ng-touched" id="input_search1611765486961"></input> */}
+            <input type="text" placeholder="Search.."></input>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
+ /* <div className="RestaurantSearch">
+      <input type="text" placeholder="Search.."></input>
+      <img src={SVG} class="search-icon" alt="search"></img>
+    </div> */
 export default SearchBar;
