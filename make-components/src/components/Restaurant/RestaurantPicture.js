6a59cc1110988f545d9b3d873f7df9eb0ca5175a
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-02-01 23:59:15 +0900

    feat: RestaurantTab - Picture Prototype Component

---
+++ make-components/src/components/Restaurant/RestaurantPicture.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const RestaurantPicture = () => {
+  return (
+    <div>
+      <img src="https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDExMDhfNTAg%2FMDAxNjA0ODI5MjM2NDIx.F-ERMZ13AG76tR53XfCALCHwqXB0nAa7Y0AximDj6nAg.sjW0-KvmfWWZG7qMv3D6_Dk2fkofZkf_lZUY1qmTWRIg.JPEG.dwk0708%2Foutput_643343784.jpg"></img>
+      <img src="https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDExMDhfNTAg%2FMDAxNjA0ODI5MjM2NDIx.F-ERMZ13AG76tR53XfCALCHwqXB0nAa7Y0AximDj6nAg.sjW0-KvmfWWZG7qMv3D6_Dk2fkofZkf_lZUY1qmTWRIg.JPEG.dwk0708%2Foutput_643343784.jpg"></img>
+      <img src="https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDExMDhfNTAg%2FMDAxNjA0ODI5MjM2NDIx.F-ERMZ13AG76tR53XfCALCHwqXB0nAa7Y0AximDj6nAg.sjW0-KvmfWWZG7qMv3D6_Dk2fkofZkf_lZUY1qmTWRIg.JPEG.dwk0708%2Foutput_643343784.jpg"></img>
+      <img src="https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDExMDhfNTAg%2FMDAxNjA0ODI5MjM2NDIx.F-ERMZ13AG76tR53XfCALCHwqXB0nAa7Y0AximDj6nAg.sjW0-KvmfWWZG7qMv3D6_Dk2fkofZkf_lZUY1qmTWRIg.JPEG.dwk0708%2Foutput_643343784.jpg"></img>
+      <img src="https://search.pstatic.net/common/?autoRotate=true&quality=95&type=w750&src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMDExMDhfNTAg%2FMDAxNjA0ODI5MjM2NDIx.F-ERMZ13AG76tR53XfCALCHwqXB0nAa7Y0AximDj6nAg.sjW0-KvmfWWZG7qMv3D6_Dk2fkofZkf_lZUY1qmTWRIg.JPEG.dwk0708%2Foutput_643343784.jpg"></img>
+    </div>
+  );
+};
+
+export default RestaurantPicture;
