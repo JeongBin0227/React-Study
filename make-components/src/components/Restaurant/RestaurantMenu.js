18538da2635f52d727237d22939ba691b20fe8a3
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-02-01 23:59:07 +0900

    feat: RestaurantTab - Menu Prototype Component

---
+++ make-components/src/components/Restaurant/RestaurantMenu.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const RestaurantMenu = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <div>
+            <span>김치볶은밥</span>
+          </div>
+          <div>
+            <span>4,500원</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span>김치볶은밥</span>
+          </div>
+          <div>
+            <span>4,500원</span>
+          </div>
+        </li>
+        <li>
+          <div>
+            <span>김치볶은밥</span>
+          </div>
+          <div>
+            <span>4,500원</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default RestaurantMenu;
