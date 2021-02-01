0242836a6c5af296b02d8a294dd4835b24cfd69e
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-02-01 23:59:01 +0900

    feat: RestaurantTab - Home Prototype Component

---
+++ make-components/src/components/Restaurant/RestaurantHome.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const RestaurantHome = () => {
+  return (
+    <div>
+      <ul>
+        <li>010-1234-4567</li>
+        <li>서울 동작구 흑석로 83</li>
+        <li>매일 11:00 ~ 23:00</li>
+        <li>김치철판볶음밥 등 최고의 맛집</li>
+      </ul>
+    </div>
+  );
+};
+
+export default RestaurantHome;
