4f25536911b7659758d846eea3682eb61f6ad981
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2021-02-01 23:59:20 +0900

    feat: RestaurantTab - Review Prototype Component

---
+++ make-components/src/components/Restaurant/RestaurantReview.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+const RestaurantReview = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <div>4/5</div>
+        </div>
+        <div>93개 (76명 참여)</div>
+        <ul>
+          <li>
+            <span></span>
+          </li>
+          <li>
+            <span></span>
+            <span>1.0</span>
+          </li>
+          <li>
+            <span></span>
+          </li>
+          <li>
+            <span></span>
+            <span>2.0</span>
+          </li>
+          <li>
+            <span></span>
+          </li>
+          <li>
+            <span></span>
+            <span>3.0</span>
+          </li>
+          <li>
+            <span></span>
+          </li>
+          <li>
+            <span></span>
+            <span>4.0</span>
+          </li>
+          <li>
+            <span></span>
+          </li>
+          <li>
+            <span></span>
+            <span>5.0</span>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <span>리뷰 23</span>
+      </div>
+      <div>
+        <ul>
+          <div>
+            <span>달고나</span>
+          </div>
+          <div>
+            <span>4</span>
+          </div>
+          <div>
+            <span>20.12.21</span>
+          </div>
+          <div>맛있지만 느끼해요</div>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantReview;
